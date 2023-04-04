56473f1c333fda5f7fe8dc3c5628031017ffd4c5
Yaroalav <Polez_ko32@student.itstep.org>
2023-04-04 22:31:31 +0300

    start project

---
+++ hw_2023_04_01/Array1D.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace hw_2023_04_01
+{
+    internal class Array1D
+    {
+    }
+}
