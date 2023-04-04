7c71d6fefc675e5f40d376e01bd024f9215f80fa
Yaroalav <Polez_ko32@student.itstep.org>
2023-04-04 23:49:32 +0300

    реализация класса Array1D

--- hw_2023_04_01/Array1D.cs
+++ hw_2023_04_01/Array1D.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,49 @@ using System.Threading.Tasks;
 
 namespace hw_2023_04_01
 {
-    internal class Array1D
+    internal class Array1D : IEnumerable
     {
+        int[] _array;
+        int _size;
+        private void CreateArray() 
+        {
+            _array = new int[_size];
+        }
+        public int Size { get { return _size; } }
+        public Array1D(int size)
+        {
+            if (size > 0)
+            {
+                _size = size;
+                CreateArray();
+            }
+        }
+        public int this[int index]
+        { 
+            get
+            {
+                if (index >= 0 && index < _size)
+                {
+                    return _array[index];
+                }
+
+                throw new IndexOutOfRangeException();
+            }
+            set
+            {
+                if (index >= 0 && index < _size)
+                {
+                    _array[index] = value;
+                }
+                else
+                {
+                    throw new IndexOutOfRangeException();
+                }
+            }
+        }
+        IEnumerator IEnumerable.GetEnumerator() 
+        {
+            return _array.GetEnumerator();
+        }
     }
 }
--- hw_2023_04_01/Program.cs
+++ hw_2023_04_01/Program.cs
@@ -10,6 +10,21 @@ namespace hw_2023_04_01
     {
         static void Main(string[] args)
         {
+            Array1D array = new Array1D(10);
+
+            Random random = new Random();
+
+            for (int i = 0; i < array.Size; i++)
+            {
+                array[i] = random.Next(-10, 10);
+            }
+
+            foreach (var item in array)
+            {
+                Console.Write($" {item}");
+            }
+
+            Console.WriteLine();
         }
     }
 }
